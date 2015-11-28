92c92ba926e141094353e835a372d424245294bb
46989414t <46989414t@iespoblenou.org>
2015-11-28 13:18:14 +0100

    programa comentado

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 
+import javafx.event.Event;
 import javafx.scene.control.*;
 
 import java.util.ArrayList;
@@ -10,86 +11,139 @@ import java.util.Map;
 
 public class Controller {
 
-    public TextField idCiudad;
-    //public TextField introUnidad;
-    public TextField idDias;
-
-    public TableView idTablaResul;
-    public TableColumn idDiaColumn;
-    public TableColumn idMaximaColumn;
-    public TableColumn idMinimaColumn;
-
-    public TextArea idTextoDia;
-    public TextArea idTextoMax;
-    public TextArea idTextoMin;
-
-    public boolean hayTexto=false;
-
-    public ArrayList<String> elementoMinList = new ArrayList<String>();
-    public int contadorMin=0;
+public TextField idCiudad;
+public TextField idDias;
+public TextField idMediaMin;
+public TextField idMediaMax;
 
 
+public TextArea idTextoDia;
+public TextArea idTextoMax;
+public TextArea idTextoMin;
 
-    ConectarApi ca = new ConectarApi();
+public Button idAceptar;
 
+public boolean hayTexto=false;
 
+ConectarApi ca = new ConectarApi();
+
+    /**
+     * al aceptar le pasa los parametros a ConectarApi con sus condiciones,
+     * sespues lee los arrayList y monta los textos de salida,
+     * tambien calcula la media extrayendo los datos del arrayList.
+     * En caso de error lanza mensaje
+     */
     public void onAceptar() {
-        System.out.println("estado texto: " + hayTexto);
-            System.out.println("acepta");
 
-            String strCiudad = idCiudad.getText();
-            String strDies = idDias.getText();
+    System.out.println("estado texto: " + hayTexto);
+        System.out.println("acepta");
 
-            ca.setCity(strCiudad);
-            ca.setDies(strDies);
+        String strCiudad = idCiudad.getText();
+        String strDies = idDias.getText();
+    if(strDies.equals("1") || strDies.equals("2") || strDies.equals("3") || strDies.equals("4") || strDies.equals("5") || strDies.equals("6") || strDies.equals("7")
+            ||strDies.equals("8") || strDies.equals("9") || strDies.equals("10") || strDies.equals("11") || strDies.equals("12") || strDies.equals("13")
+            || strDies.equals("14") || strDies.equals("15") || strDies.equals("16")){
 
-            System.out.println("city" + ca.getCity());
-            System.out.println("dies" + ca.getDies());
+        ca.setDies(strDies);
+    }
+    else{
+        idAceptar.setDisable(true);
+        idTextoDia.setText(null);
+        idTextoMax.setText(null);
+        idTextoMin.setText(null);
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Formato incorrecto");
+        alert.setHeaderText("Dias debe estar entre 1 y 16");
+        alert.setContentText("Introduzca nuevo valor");
 
-
-            //System.out.println("minima"+ca.getMinimaArrayList().size());
-            ca.getMinimaArrayList();
-            ca.getMaximaArrayList();
-            ca.getFechaArrayList();
-
-            Iterator<String> nombreIterator = ca.minimaArrayList.iterator();
-            while (nombreIterator.hasNext()) {
-                String elemento = nombreIterator.next();
-
-                idTextoMin.appendText(elemento);
-                //elementoMinList.add(elemento);
-                //idTablaResul.se
-                //idMinimaColumn.setCellFactory(elemento);
-                //contadorMin++;
-                System.out.print("Temperatura minima: " + elemento);
-
-            }
-
-            Iterator<String> nombreIterator2 = ca.maximaArrayList.iterator();
-            while (nombreIterator2.hasNext()) {
-                String elemento2 = nombreIterator2.next();
-                //elemento2.
-                idTextoMax.appendText(elemento2);
-                System.out.print("Temperatura máxima: " + elemento2);
-
-            }
-            Iterator<String> nombreIterator3 = ca.fechaArrayList.iterator();
-            //idTextoDia.clear();
-            while (nombreIterator3.hasNext()) {
-
-                String elemento3 = nombreIterator3.next();
-                idTextoDia.appendText(elemento3);
-                System.out.print("FECHA: " + elemento3);
-            }
-
+        alert.showAndWait();
 
     }
 
+        ca.setCity(strCiudad);
+       // ca.setDies(strDies);
 
-    public void onReset(ActionEvent actionEvent) {
-        String diaBorrar = idTextoDia.getText();
-        String diaBorrar1 = diaBorrar.replace(diaBorrar.substring(0, diaBorrar.length()),"dsfdsf");
-        idTextoDia.getText().;
-        System.out.println("texto para reset: "+diaBorrar1);
-    }
+
+        System.out.println("city" + ca.getCity());
+        System.out.println("dies" + ca.getDies());
+
+
+        //System.out.println("minima"+ca.getMinimaArrayList().size());
+        ca.getMinimaArrayList();
+        ca.getMaximaArrayList();
+        ca.getFechaArrayList();
+    //ca.getMinimaArrayList();
+        Iterator<String> nombreIterator = ca.minimaArrayList.iterator();
+        while (nombreIterator.hasNext()) {
+            String elemento = nombreIterator.next();
+
+            idTextoMin.appendText(elemento);
+            System.out.print("Temperatura minima: " + elemento);
+
+            float suma = 0;
+            for (String i: ca.minimaArrayList){
+                float dobI = Float.parseFloat(i);
+                suma = suma + dobI;
+            }
+            idMediaMin.setText(String.valueOf(suma/ca.minimaArrayList.size()));
+
+        }
+
+    //ca.getMaximaArrayList();
+        Iterator<String> nombreIterator2 = ca.maximaArrayList.iterator();
+        while (nombreIterator2.hasNext()) {
+            String elemento2 = nombreIterator2.next();
+            //elemento2.
+            idTextoMax.appendText(elemento2);
+            System.out.print("Temperatura máxima: " + elemento2);
+
+            float suma = 0;
+            for (String i: ca.maximaArrayList){
+                float dobI = Float.parseFloat(i);
+                suma = suma + dobI;
+            }
+            idMediaMax.setText(String.valueOf(suma/ca.maximaArrayList.size()));
+
+        }
+    //ca.getFechaArrayList();
+        Iterator<String> nombreIterator3 = ca.fechaArrayList.iterator();
+        //idTextoDia.clear();
+        while (nombreIterator3.hasNext()) {
+
+            String elemento3 = nombreIterator3.next();
+            idTextoDia.appendText(elemento3);
+            System.out.print("FECHA: " + elemento3);
+        }
+
+    //resultadoMin = ca.minimaArrayList.
+
+
+}
+
+    /**
+     * onReset
+     * resetea los cuadros para pasar otros paramentros
+     */
+    public void onReset() {
+    System.out.println("onReset");
+    idTextoDia.setText(null);
+    idTextoMax.setText(null);
+    idTextoMin.setText(null);
+
+    idDias.clear();
+    ca.minimaArrayList.clear();
+    ca.maximaArrayList.clear();
+    ca.fechaArrayList.clear();
+
+    idAceptar.setDisable(false);
+
+}
+
+    /**
+     * desactiva el botón aceptar
+     * @param event
+     */
+    public void onDeshabilitarAcep(Event event) {
+    idAceptar.setDisable(true);
+}
 }
